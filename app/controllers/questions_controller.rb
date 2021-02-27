477ed67fbaa47f80e25af3bd35eb8cae53f73760
rdenden <dddrrr814@gmail.com>
2021-02-28 04:20:16 +0900

    メソッド書き換え

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,12 +3,40 @@ class QuestionsController < ApplicationController
   end
 
   def index_q1
+    $sum1 = SumQuestion.new((rand(9)+1),(rand(9)+1))
+    $sum2 = SumQuestion.new((rand(9)+1),(rand(9)+1))
+    $sum3 = SumQuestion.new((rand(9)+1),(rand(9)+1))
+    $sum4 = SumQuestion.new((rand(9)+1),(rand(9)+1))
+    $sum5 = SumQuestion.new((rand(9)+1),(rand(9)+1))
   end
 
   def index_a1
   end
   
-  def create  
+  def create      
+    @answer1 = params[:answer1]
+    @answer2 = params[:answer2]
+    @answer3 = params[:answer3]
+    @answer4 = params[:answer4]
+    @answer5 = params[:answer5]
+    @count = 0
+    if "#{$sum1.answer}" == @answer1
+      @count += 1
+    end
+    if "#{$sum2.answer}" == @answer2
+      @count += 1
+    end
+    if "#{$sum3.answer}" == @answer3
+      @count += 1
+    end
+    if "#{$sum4.answer}" == @answer4
+      @count += 1
+    end
+    if "#{$sum5.answer}" == @answer5
+      @count += 1
+    end
+    
+      
   end
   
   def show
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,23 +1,18 @@
 module QuestionsHelper
-  def maker1
-    def text(a,b)
-      "問題文#{a} + #{b}" 
-    end
-    def answer(a,b)
-      "答え#{a + b}"
-    end
-      
-  
-  a = (rand(9)+1)
-  b = (rand(9)+1)
-   text(a,b),answer(a,b)
+end
+class SumQuestion
+    
+  def initialize(a,b)
+    @text = "#{a} + #{b}" 
+    @answer = a + b
   end
   
+  def text
+    @text
+  end
   
-  
-  def maker2
-    'aaaaa'
+  def answer
+    @answer
   end
 end
-
 
